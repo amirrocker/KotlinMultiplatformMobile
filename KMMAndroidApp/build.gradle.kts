e428e752d8e8418b13f2b98c59f451bb2ac0a964
Amir Rocker <amir.rocker@pixolus.de>
2021-01-26 17:21:51 +0100

    added some basic dependencies to start with implementing the presentation layer for android.

--- KMMAndroidApp/build.gradle.kts
+++ KMMAndroidApp/build.gradle.kts
@@ -4,12 +4,30 @@ plugins {
 }
 
 
+val materialVersion = "1.2.1"
+val appCompatVersion = "1.2.0"
+val constraintLayoutVersion = "2.0.4"
+val recyclerViewVersion = "1.1.0"
+val swipeRefreshVersion = "1.1.0"
+val cardViewVersion = "1.1.0"
+val coroutinesAndroidVersion = "1.1.0"
+val coreKtxVersion = "1.3.2"
 
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.2.1")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.2")
+
+    implementation("androidx.core:core-ktx:$coreKtxVersion")
+    implementation("com.google.android.material:material:$materialVersion")
+    implementation("androidx.appcompat:appcompat:$appCompatVersion")
+    implementation("androidx.constraintlayout:constraintlayout:$constraintLayoutVersion")
+
+    implementation("androidx.recyclerview:recyclerview:$recyclerViewVersion")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:$swipeRefreshVersion")
+    implementation("androidx.cardview:cardview:$cardViewVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesAndroidVersion")
+
+
 
 }
 
